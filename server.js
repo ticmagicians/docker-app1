1433488acaf71782836fffdca61acbd51f3cdce4
ratanbekal <ratankb@outlook.com>
2019-02-08 20:22:43 +0530

    Update server.js
    Updated Build 24

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World! from NodeJS App - Build 1.0.0.23');
+  res.send('Hello World! from NodeJS App - Build 1.0.0.24');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
